a92aeb8ed7019835c4e74f2bec0d288a68fb7369
vxv-cod <71061938+vxv-cod@users.noreply.github.com>
2024-08-11 17:33:57 +0500

    edit_cell.vue

---
+++ edit_cell.vue
@@ -0,0 +1,27 @@
+<template>
+  <span class="edit_cell"
+    @blur="val => model = val.target.textContent"
+    :contenteditable="edit"
+    >
+    {{ model }}
+  </span>
+</template>
+
+<script setup>
+defineOptions({
+  name: 'edit_cell'
+})
+const model = defineModel()
+const props = defineProps({
+  edit: {
+    type: Boolean,
+    default: () => false,
+  },
+})
+</script>
+
+<style lang="sass" scoped>
+.edit_cell
+  outline: 0
+  padding: 2px
+</style>
